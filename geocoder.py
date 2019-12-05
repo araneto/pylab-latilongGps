92210fa8f503f532cb544b226319d2ef09bc7bff
Alberto Andrade <34240578+araneto@users.noreply.github.com>
2019-12-05 13:26:32 -0300

    add comments
    Just adding some comments

--- geocoder.py
+++ geocoder.py
@@ -1,14 +1,19 @@
 __author__ = "Alberto Andrade"
 __email__ = "araneto.ctba@gmail.com"
 
+# import libs
+# geocoder: https://geocoder.readthedocs.io/
 import geocoder
 import time
 import pandas as pd
 
+# init dicts
 coordLat = []
 coordLong = []
 adresses = []
 
+# Loop over the csv file
+# 'geoDailyLimit' is the daily query limit of the google api
 with open('adresses.csv') as x:
     geoDailyLimit = 2500
     for location in x:
@@ -23,10 +28,11 @@ with open('adresses.csv') as x:
             pass
     x.close()
 
+# construct columns of dataframe
 data = {'Adress': adresses,
         'Latitude': coordLat,
         'Longitude': coordLong}
-
+# populate and saving dataframe to csv file
 df = pd.DataFrame(data=data, index=adresses, columns=['Adress', 'Latitude', 'Longitude'])
 df.to_csv('output.csv', sep='|')
 
